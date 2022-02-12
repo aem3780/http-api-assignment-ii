8979c0c3d413e068eba9d2ede239c5a77b4403cf
Allie Maus <aem3780@rit.edu>
2022-02-12 15:53:42 -0500

    finished hw with errors corrected

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -1,87 +1,81 @@
 const users = {};
 let responseCode;
 
-
 const respondJSON = (request, response, status, object) => {
-
-    const headers = {
-        'Content-Type': 'application/json',
-    };
-    response.writeHead(status, headers);
-    response.write(JSON.stringify(object));
-    response.end();
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-
-    const headers = {
-        'Content-Type': 'application/json',
-    };
-    response.writeHead(status, headers);
-    response.end();
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
-    responseCode = 200;
-    const responseJSON = {
-        users,
-    };
-    return respondJSON(request, response, responseCode, responseJSON);
+  responseCode = 200;
+  const responseJSON = {
+    users,
+  };
+  return respondJSON(request, response, responseCode, responseJSON);
 };
 
 const getUsersMeta = (request, response) => {
-    responseCode = 200;
-    respondJSONMeta(request, response, responseCode);
+  responseCode = 200;
+  respondJSONMeta(request, response, responseCode);
 };
 
-const notFound = (request, response) =>
-{
-    responseCode = 404;
-    const responseJSON = {
-        message: "The page you are looking for was not found.",
-        id: "notFound",
-    };
+const notFound = (request, response) => {
+  responseCode = 404;
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+  respondJSON(request, response, responseCode, responseJSON);
+};
 
-    respondJSON(request, response, responseCode, responseJSON);
-}
-
-const notFoundMeta = (request, resonse) =>{
-    responseCode = 404;
-    return respondJSONMeta(request, response, responseCode);
-}
+const notFoundMeta = (request, response) => {
+  responseCode = 404;
+  return respondJSONMeta(request, response, responseCode);
+};
 
 const updateUser = (request, response, body) => {
-    responseCode = 400;
-    const responseJSON = {
-        message: "Name and age are both required,", 
-    };
+  responseCode = 400;
+  const responseJSON = {
+    message: 'Name and age are both required,',
+  };
 
-    if(!body.age || !body.name){
-        responseJSON.id = 'addMissingParams';
-        return respondJSON(request, response, responseCode, responseJSON);
-    }
+  if (!body.age || !body.name) {
+    responseJSON.id = 'addMissingParams';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-        responseCode = 204;
-        if(!users[body.name]){
-            responseCode = 201;
-            users[body.name] = {};
-        }
+  responseCode = 204;
+  if (!users[body.name]) {
+    responseCode = 201;
+    users[body.name] = {};
+  }
 
-        users[body.age].age = body.age;
-        users[body.name].name = body.name;
+  users[body.name].age = body.age;
+  users[body.name].name = body.name;
 
-        if(responseCode === 201){
-            responseJSON.message = "Created Successfully.";
-            return respondJSON(request, response, responseCode, responseJSON);
-        }
-
-    return respondJSONMeta(request, response, responseCode);
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully.';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  return respondJSONMeta(request, response, responseCode);
 };
 
 module.exports = {
-    getUsers,
-    getUsersMeta,
-    updateUser,
-    notFound,
-    notFoundMeta,
-  };
+  getUsers,
+  getUsersMeta,
+  updateUser,
+  notFound,
+  notFoundMeta,
+};
